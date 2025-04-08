ece3ee874ca5644b2a5bd040267bd330ebfda045
zizobudget <zizinyahindawi@hotmail.com>
2025-04-08 23:26:18 +0200

    src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import BudgetApp from "./BudgetApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BudgetApp />
+  </React.StrictMode>
+);
